d65a0dd9b52f028e6539ce40255fcd86a6f9df33
Ira Krasylnykova <mad_gajka@mail.ru>
2021-09-10 16:10:07 +0300

    made changes to fonts

--- src/index.js
+++ src/index.js
@@ -3,13 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./components/App/App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
-import WebFont from "webfontloader";
-
-WebFont.load({
-  google: {
-    families: ["Glory", "sans-serif"],
-  },
-});
 
 ReactDOM.render(
   <React.StrictMode>
